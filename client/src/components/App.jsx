5e3056332a4a2867d03ae7f92c666f617f01510a
Vivian Quan <viviannquan@gmail.com>
2020-04-18 23:56:54 -0700

    [prog]: Added update and delete functionality to TodoItem component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,35 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoItem from './TodoItem.jsx';
-  
+
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
+    retrieveTodo()
+  }, [newTodo])
+
+  const retrieveTodo = () => {
     axios.get('/api/todos')
       .then((results) => {
         setTodos(results.data)
-      })
-      .catch((err) => console.error(err))
-  }, [])
-
-  const onSubmit = () => {
-    axios.post('./api/todos', {task: newTodo})
-      .then(() => {
-        window.alert("Added Todo!")
+        console.log(results.data)
       })
       .catch((err) => console.error(err))
   }
 
-   return(
+  const onSubmit = () => {
+    axios.post('./api/todos', { task: newTodo })
+      .then(() => {
+        retrieveTodo();
+      })
+      .catch((err) => console.error(err))
+  }
+
+  return (
     <div>
       <form onSubmit={onSubmit}>
         <input type='text' onChange={(e) => setNewTodo(e.target.value)} ></input>
         <button type='submit'>Add Todo</button>
       </form>
       {todos.map((todo, index) => {
-        return <TodoItem todo={todo} key={index} index={index} />
+        return <TodoItem todo={todo} key={index} id={todo.id} retrieveTodo={retrieveTodo} />
       })}
     </div>
   )
--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -1,21 +1,53 @@
-import React, {useState, useEffect} from 'react';
-  
-const TodoItem = (props) => {
-  const [complete, setComplete] = useState(false);
-  const [currentTask, setCurrentTask] = useState(props.todos.task)
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const done = () => {
+const TodoItem = (props) => {
+  const [complete, setComplete] = useState(props.todo.completed);
+  const [newTask, setNewTask] = useState(props.todo.task)
+  const [isClicked, setIsClicked] = useState(false);
+
+  const done = () => {
     setComplete(!complete);
   }
 
-  return(
-    <div>
-    <p>
-      <span onClick={done}>{props.todo.completed}</span>
-      <spanonClick={(e) => setCurrentTask(e.target.value)}>{props.todo.task}</span>
-    </p>
-    </div>
-  )
+  const handleUpdates = (e) => {
+    axios.put(`/api/todos/${props.id}`, { task: newTask, completed: complete })
+      .then(() => {
+        props.retrieveTodo()
+        setIsClicked(isClicked);
+      })
+      .catch((err) => console.error(err))
+  }
+
+  const deleteTask = (e) => {
+    e.preventDefault()
+    axios.delete(`/api/todos/${props.id}`)
+      .then(() => {
+        props.retrieveTodo()
+      })
+      .catch((err) => console.error(err))
+  }
+
+  useEffect(() => {
+
+  }, [setComplete, newTask])
+  {
+    return (isClicked ? (
+      <div>
+        <span>{props.todo.completed}</span>
+        <form onSubmit={handleUpdates}>
+          <input type='text' value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+          <button type='submit'>Change Task</button>
+        </form>
+      </div >
+    ) :
+      <div>
+        <button onClick={(e) => done(e)}>{complete ? 1 : 0}</button>
+        <span onClick={() => setIsClicked(!isClicked)}>{props.todo.task}</span>
+        <button onClick={deleteTask}>Delete</button>
+      </div>
+    )
+  }
 }
 
 export default TodoItem
