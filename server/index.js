2a32f8221df3240dbe85d354e23e53b25b59a204
Anthony Kim <akim2214@gmail.com>
2020-04-18 22:19:23 -0700

    [fix] corrected static path

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(parser.json());
 app.use('/api', router);
 
 // Serve the client files
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../client/dist'));
 
 // If we are being run directly, run the server.
 if (!module.parent) {
